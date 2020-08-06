8d81e8a1708f7718248fbe98b053af6c4f1d4040
h-hafezi <h.hafezi2000@gmail.com>
2020-08-06 18:13:33 +0430

    starting the game

---
+++ src/Game/Game/GamePane/GamePane.java
@@ -0,0 +1,18 @@
+package Game.Game.GamePane;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.media.MediaView;
+
+public class GamePane {
+    public MediaView media;
+    public ImageView image1;
+    public ImageView image2;
+    public ImageView image3;
+    public ImageView image4;
+    public ImageView image5;
+    public ImageView image6;
+    public ImageView image7;
+    public ImageView image8;
+    public ImageView image9;
+    
+}
